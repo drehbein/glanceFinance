f89a2360d0eac8af51bdccc53c1a92d03f9deb74
drehbein <devionrehbein@gmail.com>
2025-10-19 02:25:06 -0500

    Made layout responsive and options overlay; Closes #1

--- script.js
+++ script.js
@@ -7,26 +7,12 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('date').textContent = month + " " + year;
 });
 
-// On navigation, switches between pages
+function displayOptions () {
+  let options = document.getElementById('optionsWrapper');
+  options.classList.remove('isHidden');
+}
 
-function display (contentId, buttonID) {
-  let graph = document.getElementById("graph");
-  let breakdown = document.getElementById("breakdown");
-  let options = document.getElementById("options");
-  let graphButton = document.getElementById("graphButton");
-  let breakdownButton = document.getElementById("breakdownButton");
-  let optionsButton = document.getElementById("optionsButton");
-
-  graph.style.display = "none";
-  breakdown.style.display = "none";
-  options.style.display = "none";
-  graphButton.style.backgroundColor = "#702632"
-  breakdownButton.style.backgroundColor = "#702632"
-  optionsButton.style.backgroundColor = "#702632"
-
-  let content = document.getElementById(contentId);
-  let button = document.getElementById(buttonID);
-
-  content.style.display = "block";
-  button.style.backgroundColor = "#912F40";
+function hideOptions () {
+  let options = document.getElementById('optionsWrapper');
+  options.classList.add('isHidden');
 }
