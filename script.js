d57f0b55658b6ed240efaf7ba8c9120b360382d0
drehbein <devionrehbein@gmail.com>
2025-10-20 03:25:33 -0500

    Added breakdown table; Moved Closes #7; Closes #8

--- script.js
+++ script.js
@@ -6,10 +6,25 @@ document.addEventListener('DOMContentLoaded', () => {
   let year = date.getFullYear();
   document.getElementById('date').textContent = month + " " + year;
 
-  // Interrupts default form behavior
-  let form = document.getElementById('form');
-  form.addEventListener('submit', handleSubmit);
+  renderTable();
 });
+
+function renderTable () {
+  let tableData = JSON.parse(localStorage.getItem('tableData')) || [];
+  let tableBody = document.getElementById('breakdownBody');
+
+  tableBody.innerHTML = '';
+
+  tableData.forEach(function(tableRow, index) {
+    let tr = document.createElement('tr');
+    let unitsArray = ['Days', 'Weeks', 'Months', 'Years'];
+    tr.innerHTML += '<td>' + tableRow[0] + '</td>';
+    tr.innerHTML += '<td>' + tableRow[1] + '</td>';
+    tr.innerHTML += '<td>Every ' + tableRow[2] + ' ' + unitsArray[tableRow[3]] + '</td>';
+    tr.innerHTML += '<td><img src="images/closeIcon.svg" onclick="removeItem(' + index + ')"' + '></td>';
+    tableBody.appendChild(tr);
+  });
+}
 
 function showElements (elements) {
   elements.forEach(elementID => {
@@ -25,18 +40,33 @@ function hideElements (elements) {
   });
 }
 
-function handleSubmit (event) {
-  event.preventDefault();
-
+function addItem () {
   let tableData = JSON.parse(localStorage.getItem('tableData')) || [];
 
   let name = document.getElementById('name').value.trim();
+  let amount = document.getElementById('amount').value.trim();
   let frequency = document.getElementById('frequency').value.trim();
   let units = document.getElementById('units').value.trim();
-  let amount = document.getElementById('amount').value.trim();
-  let newRow = [name, frequency, units, amount];
+  let newRow = [name, amount, frequency, units];
 
   tableData.push(newRow);
 
   localStorage.setItem('tableData', JSON.stringify(tableData));
+
+  renderTable();
+}
+
+function removeItem (index) {
+  let tableData = JSON.parse(localStorage.getItem('tableData')) || [];
+
+  tableData.splice(index, 1);
+
+  localStorage.setItem('tableData', JSON.stringify(tableData));
+
+  renderTable();
+}
+
+function deleteBreakdownData () {
+  localStorage.removeItem('tableData');
+  renderTable();
 }
