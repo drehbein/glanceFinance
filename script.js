d1607ae0d7ebc2f3f8235d9bb960f943c384e9b0
drehbein <devionrehbein@gmail.com>
2025-10-20 01:58:12 -0500

    Added line entry for breakdown table; Closes #5

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
 
-  //populates date field based on current time
+  // Populates date field based on current time
   let date = new Date();
   let month = date.toLocaleDateString('en-US', {month: 'long'});
   let year = date.getFullYear();
   document.getElementById('date').textContent = month + " " + year;
+
+  // Interrupts default form behavior
+  let form = document.getElementById('form');
+  form.addEventListener('submit', handleSubmit);
 });
 
-function displayOptions () {
-  let options = document.getElementById('optionsWrapper');
-  options.classList.remove('isHidden');
+function showElements (elements) {
+  elements.forEach(elementID => {
+    let element = document.getElementById(elementID);
+    element.classList.remove('isHidden');
+  });
 }
 
-function hideOptions () {
-  let options = document.getElementById('optionsWrapper');
-  options.classList.add('isHidden');
+function hideElements (elements) {
+  elements.forEach(elementID => {
+    let element = document.getElementById(elementID);
+    element.classList.add('isHidden');
+  });
+}
+
+function handleSubmit (event) {
+  event.preventDefault();
+
+  let tableData = JSON.parse(localStorage.getItem('tableData')) || [];
+
+  let name = document.getElementById('name').value.trim();
+  let frequency = document.getElementById('frequency').value.trim();
+  let units = document.getElementById('units').value.trim();
+  let amount = document.getElementById('amount').value.trim();
+  let newRow = [name, frequency, units, amount];
+
+  tableData.push(newRow);
+
+  localStorage.setItem('tableData', JSON.stringify(tableData));
 }
